6865e8052a78eb522c9e0319084d75eed889fbb2
Ángel <angel.angel802@gmail.com>
2019-11-18 16:59:01 +0100

    The bar is working as expected (I hope)

--- Racket_Breakout.rkt
+++ Racket_Breakout.rkt
@@ -23,16 +23,20 @@
 (define HEIGHT 600)
 (define BAR_SPEED 8)
 (define BAR_LONG (/ WIDTH 5))
+(define INIT_BAR_X (/ WIDTH 2))
+(define INIT_BAR_Y (/ HEIGHT 1.09))
+(define INIT_BALL_X (/ WIDTH 2))
+(define INIT_BALL_Y (/ HEIGHT 1.2))
 (define INIT_BALL_SPEED 10)
 (define INIT_BALL_DIR (/ pi 2)) ;;Initial ball direction is 90º
-(define BALL_RADIUS 7.5) ;; Ball radius in pixels
+(define BALL_RADIUS 8.5) ;; Ball radius in pixels
 (define BALL_IMAGE (bitmap/file "ball.png")) ;;Ball image
 (define BACKGROUND (bitmap/file "background.png")) ;;Background image
 
 
 ;;Initial game conditions
-(define BALL (make-ball (/ WIDTH 2) (/ HEIGHT 1.2) 0 INIT_BALL_DIR)) ;;Initial ball conditions
-(define BAR (make-bar (/ WIDTH 2) (/ HEIGHT 1.09) BAR_SPEED BAR_LONG))
+(define BALL (make-ball INIT_BALL_X INIT_BALL_Y 0 INIT_BALL_DIR)) ;;Initial ball conditions
+(define BAR (make-bar INIT_BAR_X INIT_BAR_Y BAR_SPEED BAR_LONG))
 
 ;;Create the inicial world
 (define w (make-world BALL BAR #f #f))
@@ -49,45 +53,64 @@
    (place-image BALL_IMAGE (ball-x ball) (ball-y ball) background)
 )
 (define (draw-bar bar background)
-  (place-image (rectangle BAR_LONG 12 "solid" "blue") (bar-x bar) (bar-y bar) background)
+  (place-image (rectangle (bar-long bar) 12 "solid" "blue") (bar-x bar) (bar-y bar) background)
 )
 
 ;;World evolution functions
 ;;Ball wall collision manager
-(define (ball-wall-collide_checker ball)
+(define (ball-collide_checker world)
   (cond
-    ((or (>= (ball-x ball) (- WIDTH BALL_RADIUS))
-         (<= (ball-x ball) BALL_RADIUS))
-     (- pi (ball-direction ball))
+    ;;Wall collisions
+    ((or (>= (ball-x (world-ball world)) (- WIDTH BALL_RADIUS));;Vertical collision
+         (<= (ball-x (world-ball world)) BALL_RADIUS))
+     (- pi (ball-direction (world-ball world)))
     )
-    ((or (>= (ball-y ball) (- HEIGHT BALL_RADIUS))
-         (<= (ball-y ball) BALL_RADIUS))
-     (- (* 2 pi) (ball-direction ball))
+    ((or (>= (ball-y (world-ball world)) (- HEIGHT BALL_RADIUS));;Horizontal collision
+         (<= (ball-y (world-ball world)) BALL_RADIUS))
+     (- (* 2 pi) (ball-direction (world-ball world)))
     )
+    ;;Bar collisions
+    ((and (>= (ball-y (world-ball world)) (- (bar-y (world-bar world)) (+ 6 BALL_RADIUS)))
+          (<= (ball-y (world-ball world)) (+ (bar-y (world-bar world)) (+ 6 BALL_RADIUS)))
+          (>= (ball-x (world-ball world)) (- (bar-x (world-bar world)) (+ (/ (bar-long (world-bar world)) 2) BALL_RADIUS)))
+          (<= (ball-x (world-ball world)) (+ (bar-x (world-bar world)) (+ (/ (bar-long (world-bar world)) 2) BALL_RADIUS))))
+     (let
+         (;;Variables
+          (posDiff (- (ball-x (world-ball world)) (bar-x (world-bar world))))
+         )
+       ;;Cuerpo
+       (cond ;;Determine new ball direction according to the collision distance to center
+         ((<= (abs posDiff) 3) (/ pi 2));;The ball bounce perpendicular to the bar
+         ((< posDiff 0) (* (- 1 (/ (abs posDiff) (/ (bar-long (world-bar world)) 2))) (/ pi 2)));;The ball bounce with a certain angle to the right
+         ((> posDiff 0) (- pi (* (- 1 (/ (abs posDiff) (/ (bar-long (world-bar world)) 2))) (/ pi 2))));;The ball bounce with a certain angle to the left
+       )
+     )
+    )
+    ;;No collision detected
     (else #f)
   )
 )
 
-(define (ball-collide ball)
-  ;;If the ball touch a wall the direction is recalculates
-  (define new_direction (ball-wall-collide_checker ball))
+(define (ball-collide world)
+  ;;If the ball touch something the direction is recalculated
+  (define new_direction (ball-collide_checker world))
   (cond
     (new_direction
-     (make-ball (ball-x ball) (ball-y ball) (ball-speed ball) new_direction)
+     (make-ball (ball-x (world-ball world)) (ball-y (world-ball world)) (ball-speed (world-ball world)) new_direction)
     )
-    (else (make-ball (ball-x ball) (ball-y ball) (ball-speed ball) (ball-direction ball)))
+    (else (make-ball (ball-x (world-ball world)) (ball-y (world-ball world)) (ball-speed (world-ball world)) (ball-direction (world-ball world))))
   )
 )
 
 ;;Update ball state
-(define (move-ball ball)
-  (define new_ball (ball-collide ball));;Check for collisions
+(define (move-ball world)
+  (define new_ball (ball-collide world));;Check for collisions
   (make-ball (- (ball-x new_ball) (ball-Hspeed new_ball)) (- (ball-y new_ball) (ball-Vspeed new_ball)) (ball-speed new_ball) (ball-direction new_ball))
 )
 
 ;;Move bar left
 (define (move-bar-left bar)
-  (make-bar (- (bar-x bar) BAR_SPEED)
+  (make-bar (- (bar-x bar) (bar-speed bar))
             (bar-y bar)
             (bar-speed bar)
             (bar-long bar)
@@ -96,7 +119,7 @@
 
 ;;Move bar right
 (define (move-bar-right bar)
-  (make-bar (+ (bar-x bar) BAR_SPEED)
+  (make-bar (+ (bar-x bar) (bar-speed bar))
             (bar-y bar)
             (bar-speed bar)
             (bar-long bar)
@@ -107,33 +130,42 @@
 (define (move-bar world)
   (cond
     ((and (world-key_right world) (world-key_left world)) (world-bar world))
-    ((world-key_right world) (move-bar-right (world-bar world)))
-    ((world-key_left world) (move-bar-left (world-bar world)))
+    ((and (world-key_right world)
+          (<= (+ (bar-x (world-bar world)) (/ (bar-long (world-bar world)) 2)) WIDTH)
+     )
+     (move-bar-right (world-bar world)))
+    ((and (world-key_left world)
+          (>= (- (bar-x (world-bar world)) (/ (bar-long (world-bar world)) 2)) 0) 
+     )
+     (move-bar-left (world-bar world)))
     (else (world-bar world))
   )
 )
 
 ;;Manage the world evolution
 (define (progress-world world)
-  (define new_ball (move-ball (world-ball world)))
+  (define new_ball (move-ball world))
   (define new_bar (move-bar world))
 
   (make-world new_ball new_bar (world-key_right world) (world-key_left world))
 )
 
 
-;;Just for testing
+;;If the ball touch the floor the game ends
 (define (end-game? world)
-  #f
+  (cond
+    ((>= (+ (ball-y (world-ball world)) BALL_RADIUS) HEIGHT) #t)
+    (else #f)
+  )
 )
 
 
 ;;Start game
 (define (start-game world)
-  (define new_ball (make-ball (ball-x (world-ball world))
-                              (ball-y (world-ball world))
+  (define new_ball (make-ball (ball-x BALL)
+                              (ball-y BALL)
                               INIT_BALL_SPEED
-                              (ball-direction (world-ball world))
+                              (ball-direction BALL)
                    )
   )
 
@@ -143,7 +175,8 @@
 ;;Game keys manager
 (define (deal-with-guess world key)
   (cond
-        ((and (key=? key " ") (= (ball-speed (world-ball world)) 0)) (start-game world))
+       ;;(and (key=? key " ") (= (ball-speed (world-ball world)) 0))
+        ((key=? key " ") (start-game world))
         [(key=? key "left") (make-world (world-ball world) (world-bar world) (world-key_right world) #t)]
         [(key=? key "right") (make-world (world-ball world) (world-bar world) #t (world-key_left world))]
         (else world)
